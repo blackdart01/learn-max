9db090fa987eaf82aaabc5a76f97a1b5e6350c1d
srijan <evilblackdart@gmail.com>
2025-04-16 00:52:49 +0530

    changes updated

--- server.js
+++ server.js
@@ -1,29 +1,3 @@
-// // backend/server.js
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const cors = require('cors');
-// require('dotenv').config();
-
-// const app = express();
-// const port = process.env.PORT || 5000;
-
-// app.use(cors());
-// app.use(express.json()); // For parsing JSON request bodies
-
-// const uri = process.env.ATLAS_URI; // Your MongoDB connection string from .env
-// mongoose.connect(uri) // Remove useNewUrlParser and useUnifiedTopology
-//     .then(() => console.log('MongoDB database connection established successfully'))
-//     .catch(err => console.log(err));
-
-// app.get('/', (req, res) => {
-//     res.send('Backend server is running!');
-// });
-
-// app.listen(port, () => {
-//     console.log(`Server is running on port: ${port}`);
-// });
-
-// backend/server.js
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -39,7 +13,6 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri)
     .then(() => console.log('MongoDB database connection established successfully'))
